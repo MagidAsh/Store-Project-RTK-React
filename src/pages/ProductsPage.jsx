08805baf68aa797e8217e929704b195592721dd6
Majid Ash <Magid.Ash.Si@gmail.com>
2024-08-25 10:46:32 -0700

    Rendering the product list

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,21 @@
+import { useProducts } from "../context/ProductContext";
 
+import styles from "./ProductsPage.module.css";
 
 function ProductsPage() {
-  return <div>ProductsPage</div>;
+  const products = useProducts();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.products}>
+        {!products.length && <p>Loading...</p>}
+        {products.map((product) => (
+          <p key={product.id}>{product.title}</p>
+        ))}
+      </div>
+      <div>sidebar</div>
+    </div>
+  );
 }
 
 export default ProductsPage;
