aab3dfb3c9df34ddfbe67a66a1c184df29cec967
Majid Ash <Magid.Ash.Si@gmail.com>
2024-08-26 03:45:11 -0700

    Create a search field

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,23 +1,50 @@
-import { useProducts } from "../context/ProductContext";
-
-import styles from "./ProductsPage.module.css";
+import { useState } from "react";
+import { ImSearch } from "react-icons/im";
 
 import Card from "../components/Card";
 import Loader from "../components/Loader";
 
+import { useProducts } from "../context/ProductContext";
+
+import styles from "./ProductsPage.module.css";
+
 function ProductsPage() {
   const products = useProducts();
 
+  const [search, setSearch] = useState("");
+
+  const searchHandler = (event) => {
+    setSearch(event.target.value.toLowerCase().trim());
+    console.log(search);
+  };
+
+  const submitHandler = () => {
+    console.log("search");
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {!products.length && <Loader />}
-        {products.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={searchHandler}
+        />
+        <button onClick={submitHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && <Loader />}
+          {products.map((product) => (
+            <Card key={product.id} data={product} />
+          ))}
+        </div>
+        <div>sidebar</div>
+      </div>
+    </>
   );
 }
 
